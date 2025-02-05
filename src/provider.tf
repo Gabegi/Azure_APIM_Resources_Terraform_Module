b798237e208b0c9b4ac4b2884f24082ce82bb666
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-05 09:35:51 +0100

    changing vars

--- src/provider.tf
+++ src/provider.tf
@@ -16,5 +16,19 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+resource "azurerm_resource_group" "rg" {
+  name     = "my-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_api_management" "apim" {
+  name                = "my-apim"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+    sku_name = "Consumption_0"
+}
 
 
